420d45d75366dadcded0d68720441624e84fc795
kylehackett99 <50998448+kylehackett99@users.noreply.github.com>
2022-04-21 18:44:38 -0400

    Solved Null Error bug on load with no files in localStorage

--- hourwork/src/index.js
+++ hourwork/src/index.js
@@ -7,60 +7,65 @@ import {Card} from './js/Card';
 import {Node} from './js/Node'
 import {MindmapObj} from './js/MindmapObj';
 
+
 const App = () => {
 
   // retrieve nestedArray and title from localStorage
   var retrievedData = localStorage.getItem("file-array");
   var nestedArray = JSON.parse(retrievedData);
   var title = localStorage.getItem("file-name");
-
+  
   // initialize our graph
   var appController = new MindmapObj();
 
-  // title card
-  var titleCard = new Node(0, new Card(title));
-  appController.setTitle(titleCard.getLabel());
+  // If the nested array is null -> then it wont parse the data in local storage
+  if(nestedArray != null){
+    // title card
+    var titleCard = new Node(0, new Card(title));
+    appController.setTitle(titleCard.getLabel());
 
-  //sets dueDate for the MindMap
-  appController.setDueDate(localStorage.getItem("due-date"));
+    //sets dueDate for the MindMap
+    appController.setDueDate(localStorage.getItem("due-date"));
 
-  // empty array for the parent nodes
-  var parents = [];
-  // counter for the node id #'s
-  var id_counter = 0;
-  // temp variable for each child node
-  var child;
+    // empty array for the parent nodes
+    var parents = [];
+    // counter for the node id #'s
+    var id_counter = 0;
+    // temp variable for each child node
+    var child;
 
-  /* loop through the top layer of the nested array and fill in the parent
-  * nodes */
-  for (var i = 0; i < nestedArray.length; i++) {
-    id_counter++;
-    parents[i] = new Node(id_counter, new Card(nestedArray[i][0], nestedArray[i][1]));
-  }
-
-  /* make the titleCard the vertex, then loop through and make each parent
-  * node an edge to that card */
-  appController.addNode(titleCard);
-  for (var i = 0; i < nestedArray.length; i++) {
-    appController.addNode(parents[i]);
-    appController.addEdge(titleCard, parents[i]);
-  }
-
-  /* go through the children and add them as edges to their parent nodes */
-  for (var i = 0; i < nestedArray.length; i++) {
-    for (var j = 2; j < nestedArray[i].length; j++) {
+    /* loop through the top layer of the nested array and fill in the parent
+    * nodes */
+    for (var i = 0; i < nestedArray.length; i++) {
       id_counter++;
-      child = new Node(id_counter, new Card(nestedArray[i][j][0], nestedArray[i][j][1]));
-      appController.addEdge(parents[i], child);
+      parents[i] = new Node(id_counter, new Card(nestedArray[i][0], nestedArray[i][1]));
     }
+
+    /* make the titleCard the vertex, then loop through and make each parent
+    * node an edge to that card */
+    appController.addNode(titleCard);
+    for (var i = 0; i < nestedArray.length; i++) {
+      appController.addNode(parents[i]);
+      appController.addEdge(titleCard, parents[i]);
+    }
+
+    /* go through the children and add them as edges to their parent nodes */
+    for (var i = 0; i < nestedArray.length; i++) {
+      for (var j = 2; j < nestedArray[i].length; j++) {
+        id_counter++;
+        child = new Node(id_counter, new Card(nestedArray[i][j][0], nestedArray[i][j][1]));
+        appController.addEdge(parents[i], child);
+      }
+    }
+
+    // Generates deck of cards from the Mindmap
+    appController.generateDeck();
   }
-  // Generates deck of cards from the Mindmap
-  appController.generateDeck();
 
   console.log(appController);
 
-   // Handles Logic for when buttons are pressed
-   function handleNext(e) {
+  // Handles Logic for when buttons are pressed
+  function handleNext(e) {
     e.preventDefault();
     appController.nextCard();
     //console.log(appController);
@@ -124,7 +129,7 @@ const App = () => {
         <button onClick={handlePrevious}>Prev</button>
         <Flashcard/>
         <h4 id="temp"></h4>
-        <button onClick={handleNext}>Next</button>
+        <button onClick={handleNext}>Next</button> 
       </div>
       <div>
         <MindMap/>
